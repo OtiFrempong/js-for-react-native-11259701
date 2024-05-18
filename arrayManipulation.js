76f8bedd9b7b25285b5581b7976102956bd25372
Serwaa19 <motifrempong@gmail.com>
2024-05-18 14:54:34 +0000

    This commit introduces a new function `formatArrayStrings` in `arrayManipulation.js`.
    The function takes two arrays:
    
    * An array of strings.
    * An array of numbers processed by `processArray`.
    
    It iterates through both arrays and modifies each string based on its corresponding number:
    
    * If the number is even, the entire string is capitalized.
    * If the number is odd, the string is converted to lowercase.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,15 +1,33 @@
 function processArray(arr) {
   return arr.map(num => {
-      if (num % 2 === 0) {
-         
+      if (num % 2 === 0) {     
           return num * num;
-      } else {
-          
+      } else {    
           return num ** 3;
       }
   });
 }
 
+
+function formatArrayStrings(strings, number) {
+    if (strings.length !== numbers.length) {
+        throw new Error(']Both arrays must have the same length');
+    }
+
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+
 const numbers = [1, 2, 3, 4, 5];
 const processedNumbers = processArray(numbers);
 console.log(processedNumbers); 
+
+const strings = ["My", "First", "JS", "Code", "Yayy!"];
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(formattedStrings);
